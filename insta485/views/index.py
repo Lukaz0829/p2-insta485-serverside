4bcdad3fdedc1b633dfa125e144fd744ae1e3654
Lucas Sun <lucassun@umich.edu>
2023-09-15 16:42:44 -0400

    Finish index

--- insta485/views/index.py
+++ insta485/views/index.py
@@ -6,6 +6,7 @@ URLs include:
 """
 import flask
 import insta485
+import arrow
 
 
 @insta485.app.route('/')
@@ -17,14 +18,70 @@ def show_index():
 
     # Query database
     logname = "awdeorio"
-    cur = connection.execute(
+    users = connection.execute(
         "SELECT username, fullname "
         "FROM users "
         "WHERE username != ?",
         (logname, )
     )
-    users = cur.fetchall()
+    users = users.fetchall()
 
-    # Add database info to context
-    context = {"users": users}
+    following_posts = (
+        "SELECT p.postid, p.filename, p.owner, p.created "
+        "FROM posts p "
+        "JOIN following f ON f.username1 = ? AND f.username2 = p.owner "
+    )
+    user_posts = (
+        "SELECT p.postid, p.filename, p.owner, p.created "
+        "FROM posts p "
+        "WHERE p.owner = ? "
+    )
+    final_query = f"{following_posts} UNION {user_posts} ORDER BY created DESC"
+
+    posts = connection.execute(final_query, (logname, logname))
+    posts = posts.fetchall()
+
+    for post in posts:
+        post_id = post['postid']
+        post_owner = post['owner']
+        print(post_owner)
+
+        owner = connection.execute(
+            "SELECT filename FROM users WHERE username = ?", 
+            (post_owner,)
+        )
+        owner = owner.fetchone()
+        post['owner_img_url'] = owner['filename']
+
+        likes = connection.execute(
+            "SELECT * FROM likes WHERE postid = ?", 
+            (post_id,)
+        )
+        likes = likes.fetchall()
+        post['likes'] = len(likes)
+
+        current_liked = connection.execute(
+            "SELECT * FROM likes WHERE postid = ? AND owner = ?", 
+            (post_id, logname)
+        )
+        if current_liked:
+            post['current_liked'] = True
+        else:
+            post['current_liked'] = False
+
+        comments = connection.execute(
+            "SELECT owner, text FROM comments WHERE postid = ? ORDER BY created ASC", 
+            (post_id,)
+        )
+        comments = comments.fetchall()
+        post['comments'] = comments
+
+        time = arrow.get(post['created']).to('US/Eastern')
+        post['created'] = time.humanize()
+        # print(posts)
+
+    context = {
+        "users": users,
+        "posts": posts
+    }
     return flask.render_template("index.html", **context)
